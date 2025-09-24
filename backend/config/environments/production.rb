3b63d6b0a5115d38626d331a3539bea5ed46271e
Arliiii <arlindauruci75@gmail.com>
2025-09-24 22:04:22 +0200

    added hosts

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -77,11 +77,11 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
+  # Allow requests from Render.com hostnames
+  config.hosts << "proje-1-staj.onrender.com"
+  # Allow requests from any onrender.com subdomain for flexibility
+  config.hosts << /.*\.onrender\.com/
+  
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
